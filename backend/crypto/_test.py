b212c6d872796573e2297f9765f3ce9fa6a58d67
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-08-08 21:59:54 +0100

    Optimize the logic

--- backend/crypto/_test.py
+++ backend/crypto/_test.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-from backend.crypto.key import _export_private_key, _import_private_key, generate_key_pair, _export_public_key, _import_public_key
+from backend.crypto.key import import_private_key, generate_key_pair, import_public_key, export_key
 
 if __name__ == "__main__":
     pri, pub = generate_key_pair()
-    _export_private_key(pri, Path() / "pri.pem")
-    pri_new = _import_private_key(Path() / "pri.pem")
-    _export_public_key(pub, Path() / "pub.pem")
-    pub_new = _import_public_key(Path() / "pub.pem")
+    export_key(pri, Path() / "pri.pem")
+    pri_new = import_private_key(Path() / "pri.pem")
+    export_key(pub, Path() / "pub.pem")
+    pub_new = import_public_key(Path() / "pub.pem")
