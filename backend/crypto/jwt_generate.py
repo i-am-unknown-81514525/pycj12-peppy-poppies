7f922f170e1139075f7d20fc178930d8baa41bdd
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-08-09 02:16:15 +0100

    More ruff reformat

--- backend/crypto/jwt_generate.py
+++ backend/crypto/jwt_generate.py
@@ -12,17 +12,24 @@ if TYPE_CHECKING:
 
 type JSON = dict[str, JSON | list[JSON] | str | float | int | bool | None]
 
+
 class JWTGenerator:
     """Generates JSON Web Tokens (JWTs) signed with an Ed25519 private key."""
+
     def __init__(self, issuer: str, private_key: Ed25519PrivateKey) -> None:
         self._issuer: str = issuer
         self._priv: bytes = get_pem(private_key)
 
     def generate(
-        self, *, website: str, session_id: str, challenge_id: str, valid_duration: float = 600, **kwargs : JSON,
+        self,
+        *,
+        website: str,
+        session_id: str,
+        challenge_id: str,
+        valid_duration: float = 600,
+        **kwargs: JSON,
     ) -> str:
         """Generate JWT token based on website, session_id and challenge_id and any addition attributes."""
-
         data = {
             **kwargs,
             "session_id": session_id,
