35995969f1394a282c68b967fe52f9d0331310b2
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-08-12 21:38:54 +0100

    Attempt to add UI with panel

--- frontend/static/captcha.py
+++ frontend/static/captcha.py
@@ -7,10 +7,10 @@ from pyodide.ffi import create_proxy
 from pyodide.http import pyfetch
 from pyscript import document, window, display, when  # type: ignore[reportAttributeAccessIssue]
 import panel as pn
+import param
 
 body = document.body
 worker = window.Worker.new("/static/runner.js", {"type": "module"})
-status: dict[str, int] = {}  # -1: failed, 0: success, 1: starting, 2: loading code, >=100: running test (idx:v-100)
 
 class GetChallengeResponse(TypedDict):
     """Response schema for /get_challenge endpoint."""
@@ -53,17 +53,22 @@ async def worker_on_message(e) -> None:
     challenge_id = get_challenge_id()
     if key == "result":
         result = await send_result(json.loads(value))
-        status[challenge_id] = 0 if result else -1
+        progress_bar.value = progress_bar.max
+        if result:
+            progress_bar.bar_color = "success"
+        else:
+            progress_bar.bar_color = "danger"
     elif key == "load":
-        status[challenge_id] = 2
+        progress_bar.value = 1
     elif key == "run":
-        status[challenge_id] = 100 + int(value)
+        progress_bar.value = 1 + int(value)
+    elif key == "pyodide-loaded":
+        loaded_item.has_loaded = True
 
 
 async def submit(code: str, task: list[int]) -> None:
     """Submit the code to be executed locally with the given task."""
     challenge_id = get_challenge_id()
-    status[challenge_id] = 1
     worker.postMessage(json.dumps({"code": code, "task": task}))
 
 
@@ -81,6 +86,7 @@ async def send_result(results: list[int]) -> bool:
     jwt = await response.text()
     splitted = jwt.split(".")
     if len(splitted) != 3:  # noqa: PLR2004
+        submit_button.disabled = False
         return False
     payload_str = b64decode(splitted[1] + "=" * (4 - len(splitted[1]) % 4)).decode()
     payload: SolutionCorrectJWTPayload = json.loads(payload_str)
@@ -93,3 +99,64 @@ async def send_result(results: list[int]) -> bool:
 window.get_challenge = create_proxy(get_challenge)
 window.submit = create_proxy(submit)
 worker.onmessage = create_proxy(worker_on_message)
+
+class PyodideHasLoaded(param.Parameterized):
+    has_loaded = param.Boolean(precedence=-1.0)
+
+    @param.depends("has_loaded")
+    def render(self):
+        if self.has_loaded:  # type: ignore[reportGeneralTypeIssues]
+            return initial_verify
+        else:
+            return pn.indicators.LoadingSpinner(size=20, value=True, color="secondary", bgcolor='light')
+
+
+loaded_item = PyodideHasLoaded()
+initial_verify = pn.widgets.Button(name='Verify', button_type='primary')
+question = pn.pane.Str("")
+question_loading = pn.indicators.LoadingSpinner(size=20, value=True, color="secondary", bgcolor='light', visible=False)
+code_editor = pn.widgets.CodeEditor(value="""
+def calc(x: int) -> int:
+    # Your implementation here
+    pass
+""", language='python', theme='monokai', name="Put your solution here:")
+submit_button = pn.widgets.Button(name='Submit', button_type='primary', visible=False)
+progress_bar = pn.indicators.Progress(name='Progress', value=0, width=200, max=3, bar_color="primary")
+tasks: list[int] = []
+
+async def click_initial_verify(_):
+    global tasks
+    initial.visible = False
+    question_loading.visible = True
+    question_str, tasks = await get_challenge()
+    question.object = question_str
+    question_loading.visible = False
+    after.visible = True
+    progress_bar.max = len(tasks) + 2
+
+
+async def click_submit():
+    code_string: str = code_editor.value  # type: ignore[reportAssignmentType]
+    submit_button.disabled = True
+    await submit(code_string, tasks)
+
+
+initial_verify.on_click(click_initial_verify)
+
+initial = pn.Row(
+    pn.pane.Str("Very for are human"),
+    pn.panel(loaded_item)
+)
+
+after = pn.Column(
+    question,
+    code_editor,
+    progress_bar,
+    submit_button
+)
+
+pn.Column(
+    initial,
+    question_loading,
+    after
+).servable(target="captcha")
