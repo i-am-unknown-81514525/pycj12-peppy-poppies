273ed01af75ad42fe64405e916373cbb6abbbba5
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-08-13 03:29:41 +0100

    Add fix

--- frontend/static/runner.js
+++ frontend/static/runner.js
@@ -5,16 +5,17 @@ let pyodide = await loadPyodide();
 console.log("[Worker] Complete pyodide load");
 self.onmessage = function (e) {
     let json_value = JSON.parse(e.data);
-    let code = json_value.code;
-    let test_value = json_value.task;
+    console.log(json_value);
+    let code = json_value["code"];
+    let test_value = json_value["task"];
     const dict = pyodide.globals.get("dict"); // https://github.com/pyodide/pyodide/issues/703#issuecomment-1937774811
     const globals = dict();
     let results = [];
-    this.pyodide.runPython(code, { globals, locals: globals });
+    pyodide.runPython(code, { globals, locals: globals });
     postMessage("load;0");
     for (let i = 0; i < test_value.length; i++) {
         let value = test_value[i];
-        let result = this.pyodide.runPython(`calc(${value})`, {
+        let result = pyodide.runPython(`calc(${value})`, {
             globals,
             locals: globals,
         });
