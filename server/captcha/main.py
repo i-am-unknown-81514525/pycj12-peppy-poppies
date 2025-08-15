107449892edadb3003c695eefc7c3bd404183e43
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-08-15 21:30:36 +0100

    Fix linting

--- server/captcha/main.py
+++ server/captcha/main.py
@@ -1,22 +1,22 @@
 from os import getenv
 from pathlib import Path
 
-from msgspec.json import encode, decode
+import sympy  # noqa: F401 preload sympy
 from advanced_alchemy.exceptions import DuplicateKeyError, NotFoundError, RepositoryError
-from litestar.datastructures import State
 from crypto.key import export_all, generate_key_pair, import_all
 from litestar import Litestar
 from litestar.config.cors import CORSConfig
+from litestar.datastructures import State
 from litestar.exceptions import ClientException, NotAuthorizedException, NotFoundException
 from litestar.logging import LoggingConfig
 from litestar.openapi import OpenAPIConfig
 from litestar.openapi.plugins import ScalarRenderPlugin
 from litestar.static_files import create_static_files_router
+from msgspec.json import decode, encode
 from server.captcha.controller.challenge import ChallengeController
 from server.captcha.lib.config import alchemy_plugin
 from server.captcha.lib.utils import exception_handler
-from server.captcha.schema import questions
-from server.captcha.schema.questions import QuestionSet, Question
+from server.captcha.schema.questions import Question, QuestionSet
 
 CONFIG_PATH = Path(getenv("KEY_PATH", "./captcha_data"))
 
@@ -29,18 +29,28 @@ def ensure_key(app: Litestar) -> None:  # noqa: D103
     app.state["pub_key"] = pub
     app.state["pri_key"] = pri
 
+
 def ensure_questions(app: Litestar) -> None:  # noqa: D103
     if not (CONFIG_PATH / "question_set.json").exists():
         with (CONFIG_PATH / "question_set.json").open("wb") as fp:
-            fp.write(encode(
-                QuestionSet(
-                    construct=["{init} {dyn:base}"],
-                    base=[Question(question="{y}+x", validator="validator=lambda x:{y}+x", range={"y": (-65536, 65536)}, input=(1, 65536))],
-                    part=[],
-                    init=["Write a function `calc(x: int) -> int` to calculate"],
-                    cont=[", then"]
-                )
-            ))
+            fp.write(
+                encode(
+                    QuestionSet(
+                        construct=["{init} {dyn:base}"],
+                        base=[
+                            Question(
+                                question="{y}+x",
+                                validator="validator=lambda x:{y}+x",
+                                range={"y": (-65536, 65536)},
+                                input=(1, 65536),
+                            ),
+                        ],
+                        part=[],
+                        init=["Write a function `calc(x: int) -> int` to calculate"],
+                        cont=[", then"],
+                    ),
+                ),
+            )
     with (CONFIG_PATH / "question_set.json").open("rb") as fp:
         question_set = decode(fp.read(), type=QuestionSet)
     app.state["question_set"] = question_set
@@ -81,5 +91,5 @@ app = Litestar(
         RepositoryError: exception_handler,
     },
     cors_config=CORSConfig(allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]),
-    state=State()
+    state=State(),
 )
