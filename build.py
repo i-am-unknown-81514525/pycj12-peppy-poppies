13ff599d0aa0a731b272cb46242d700b915d03cc
Harshal Laheri <harshallaheri12@gmail.com>
2025-08-15 18:36:06 -0400

    small fixes

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ from shutil import copytree, rmtree
 try:
     from dotenv import load_dotenv
 
-    load_dotenv()
+    load_dotenv(override=True)
 except ImportError:
     pass
 
@@ -15,21 +15,21 @@ copytree(Path("./frontend"), "./dist/frontend")
 index_html_path: Path = Path("./dist") / "frontend" / "demo" / "index.html"
 
 if index_html_path.exists():
-    with index_html_path.open("r") as fp:
+    with index_html_path.open("r", encoding="utf-8") as fp:
         content = fp.read()
 
     content = content.replace("[domain]", getenv("CODECAPTCHA_DOMAIN", "localhost:8001"))
 
-    with index_html_path.open("w") as fp:
+    with index_html_path.open("w", encoding="utf-8") as fp:
         fp.write(content)
 
 app_js_path: Path = Path("./dist") / "frontend" / "demo" / "app.js"
 
-if index_html_path.exists():
-    with index_html_path.open("r") as fp:
+if app_js_path.exists():
+    with app_js_path.open("r", encoding="utf-8") as fp:
         content = fp.read()
 
     content = content.replace("[domain]", getenv("CODECAPTCHA_DOMAIN", "localhost:8001"))
 
-    with index_html_path.open("w") as fp:
+    with app_js_path.open("w", encoding="utf-8") as fp:
         fp.write(content)
