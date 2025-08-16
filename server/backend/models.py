f8b5c5fa73726f2f8505611d2d8ce9bbeb72fa1c
Harshal Laheri <harshallaheri12@gmail.com>
2025-08-16 12:05:06 -0400

    update models

--- server/backend/models.py
+++ server/backend/models.py
@@ -1,11 +1,11 @@
 from advanced_alchemy.base import UUIDAuditBase
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class User(UUIDAuditBase):
     """Represents a user in the system."""
 
-    username: Mapped[str]
+    username: Mapped[str] = mapped_column(unique=True, nullable=False, index=True)
     password: Mapped[str]
 
     def __repr__(self) -> str:
